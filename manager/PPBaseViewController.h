281855c2c209b788d3b9ad2af47c0673dc07578c
zhanggeng <m13716920635@163.com>
2016-12-16 13:13:46 +0800

    llll

---
+++ manager/PPBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPBaseViewController.h
+//  rongchat
+//
+//  Created by Donald on 16/12/16.
+//  Copyright © 2016年 vd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPBaseViewController : UIViewController
+
+@end
