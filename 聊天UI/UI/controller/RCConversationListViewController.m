44a1ff703b4276c2d909a564050183fd872f0a29
zhanggeng <m13716920635@163.com>
2017-09-02 19:36:42 +0800

    xiuga

--- 聊天UI/UI/controller/RCConversationListViewController.m
+++ 聊天UI/UI/controller/RCConversationListViewController.m
@@ -133,7 +133,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 60;
+    return 55;
 }
 
 @end
--- 聊天UI/聊天会话cell/RCConversationListCell.m
+++ 聊天UI/聊天会话cell/RCConversationListCell.m
@@ -8,13 +8,36 @@
 
 #import "RCConversationListCell.h"
 #import "RCThemeDefine.h"
+#import "UIImage+RCIMExtension.h"
+#import "PPViewUtil.h"
+
+
+@interface RCIMRemindLabel : UILabel
+
+@end
+
+@implementation RCIMRemindLabel
+
+- (CGSize)intrinsicContentSize
+{
+    if (!self.text.length) {
+        return CGSizeZero;
+    } else {
+        CGSize size = [PPViewUtil sizeWithString:self.text font:self.font constrainedToSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX) lineBreakMode:NSLineBreakByCharWrapping];
+        self.layer.cornerRadius = MIN(size.height,size.width) * 0.5;
+        return CGSizeMake(size.width+6, size.height);
+    }
+}
+
+@end
+
 @interface RCConversationListCell ()
 
 @property (nonatomic,strong) UIImageView * avaturImageView;//接受者的头像
 @property (nonatomic,strong) UILabel * receiveLabel;// 接受者的名字
 @property (nonatomic,strong) UILabel * contentLabel;// 最后的一条消息
 @property (nonatomic,strong) UILabel * timeLabel;//受到最后一条消息的时间
-
+@property (nonatomic,strong) RCIMRemindLabel * reamindLabel;
 @end
 
 @implementation RCConversationListCell
@@ -24,10 +47,6 @@
     
     // Initialization code
 }
-
-
-
-
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
@@ -45,45 +64,43 @@
 }
 - (void)createUI
 {
-    self.avaturImageView = [UIImageView new];
+    
     [self.contentView addSubview:self.avaturImageView];
     [self.avaturImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.contentView.mas_left).mas_offset(15);
+        make.left.mas_equalTo(self.contentView.mas_left).mas_offset(10);
         make.centerY.mas_equalTo(self.contentView.mas_centerY);
-        make.top.mas_equalTo(self.contentView.mas_top);
+        make.top.mas_equalTo(self.contentView.mas_top).mas_offset(10);
         make.width.mas_equalTo(self.avaturImageView.mas_height);
     }];
-    self.timeLabel = [UILabel new];
     [self.contentView addSubview:self.timeLabel];
     [self.timeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.right.mas_equalTo(self.contentView.mas_right).mas_offset(-15);
-        make.width.lessThanOrEqualTo(@200);
         make.height.mas_equalTo(15);
         make.top.mas_equalTo(self.avaturImageView.mas_top);
     }];
-    self.timeLabel.textAlignment = NSTextAlignmentRight;
-    self.timeLabel.textColor = UIColorFromRGB(0xa2a2a2);
-    self.timeLabel.font = [UIFont systemFontOfSize:11];
-    self.receiveLabel = [UILabel new];
     [self.contentView addSubview:self.receiveLabel];
     [self.receiveLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.avaturImageView.mas_right).mas_offset(5);
-        make.top.mas_equalTo(self.avaturImageView.mas_top);
+        make.left.mas_equalTo(self.avaturImageView.mas_right).mas_offset(8);
+        make.top.mas_equalTo(self.avaturImageView.mas_top).mas_offset(4);
         make.height.mas_equalTo(15);
-        make.right.mas_equalTo(self.timeLabel.mas_left).mas_offset(-10);
+        make.right.lessThanOrEqualTo(self.timeLabel.mas_left).mas_offset(-10);
     }];
     
-    self.contentLabel = [UILabel new];
     [self.contentView addSubview:self.contentLabel];
     [self.contentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(self.receiveLabel.mas_left);
-        make.bottom.mas_equalTo(self.contentView.mas_bottom).mas_offset(-5);
-        make.height.mas_equalTo(15);
+        make.top.mas_equalTo(self.receiveLabel.mas_bottom).mas_offset(4);
         make.width.mas_equalTo(self.receiveLabel.mas_width);
     }];
-    self.receiveLabel.font = [UIFont systemFontOfSize:14];
-    self.contentLabel.font = [UIFont systemFontOfSize:12];
-    self.contentLabel.textColor = UIColorFromRGB(0xa2a2a2);
+   
+    [self.contentView addSubview:self.reamindLabel];
+    [self.reamindLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.avaturImageView.mas_right);
+        make.centerY.mas_equalTo(self.avaturImageView.mas_top);
+        make.width.mas_greaterThanOrEqualTo(self.reamindLabel.mas_height).multipliedBy(1);
+    }];
+    
+    
 
 }
 
@@ -94,10 +111,72 @@
         RCTextMessage * message = (RCTextMessage *)conversation.lastestMessage;
         self.contentLabel.text = message.content;
         [[PPTUserInfoEngine shareEngine]queryUserInfoWithUserId:conversation.targetId resultCallback:^(RCUserInfoData *userInfo) {
-            [self.avaturImageView sd_setImageWithURL:[NSURL URLWithString:userInfo.user.portraitUri]];
+            UIImage * avatarImage = RCIM_PLACE_ARATARIMAGE;
+            SD_LOADIMAGE(self.avaturImageView, userInfo.user.portraitUri,avatarImage);
             self.receiveLabel.text = userInfo.user.name;
         }];
     }
+    self.reamindLabel.text = @"10";
 }
 
+
+- (UIImageView *)avaturImageView
+{
+    if(_avaturImageView==nil)
+    {
+        _avaturImageView = [UIImageView new];
+        _avaturImageView.layer.cornerRadius = 2;
+        _avaturImageView.layer.masksToBounds = YES;
+    }
+    return _avaturImageView;
+}
+
+
+- (RCIMRemindLabel *)reamindLabel
+{
+    if(_reamindLabel==nil)
+    {
+        _reamindLabel = [RCIMRemindLabel new];
+        _reamindLabel.backgroundColor = RCIM_REMIND_COLOR;
+        _reamindLabel.font = [UIFont systemFontOfSize:12];
+        _reamindLabel.layer.masksToBounds = YES;
+        _reamindLabel.textColor = [UIColor whiteColor];
+        _reamindLabel.textAlignment = NSTextAlignmentCenter;
+    }
+    return _reamindLabel;
+}
+
+- (UILabel *)contentLabel
+{
+    if(_contentLabel==nil)
+    {
+        _contentLabel = [UILabel new];
+        _contentLabel.font = [UIFont systemFontOfSize:13];
+        _contentLabel.textColor = UIColorFromRGB(0xa2a2a2);
+    }
+    return _contentLabel;
+}
+
+
+- (UILabel *)timeLabel
+{
+    if(_timeLabel==nil)
+    {
+        _timeLabel = [UILabel new];
+        _timeLabel.textAlignment = NSTextAlignmentRight;
+        _timeLabel.textColor = UIColorFromRGB(0xa2a2a2);
+        _timeLabel.font = [UIFont systemFontOfSize:12];
+    }
+    return _timeLabel;
+}
+
+- (UILabel *)receiveLabel
+{
+    if(_receiveLabel==nil)
+    {
+        _receiveLabel = [UILabel new];
+        _receiveLabel.font = [UIFont systemFontOfSize:15];
+    }
+    return _receiveLabel;
+}
 @end
